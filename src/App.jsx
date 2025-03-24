471ab8703606d23593c310a7d350fc67a711b762
NanoSf <nano.softm@gmail.com>
2025-03-23 19:55:19 -0500

    App BrowserRouter añadido --TEAM suavitel :see_no_evil:

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Sidebar from './Components/Sidebar'
 import './App.css'
+import { BrowserRouter } from "react-router";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
       <Sidebar />
+    </BrowserRouter>
     </>
   )
 }
