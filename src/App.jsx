e56b464d5c61b293d0f5f1fc59748d3aff159813
NanoSf <nano.softm@gmail.com>
2025-03-23 22:45:55 -0500

    Componente Holidays --TEAM suavitel :see_no_evil:

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Sidebar from './Components/Sidebar'
 import './App.css'
 import { BrowserRouter } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
